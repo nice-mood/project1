75885139c8d8adb7a5dc4fb1cff7adc39220bb56
Unknown <heli.isohatala@gmail.com>
2017-03-13 10:54:40 +0200

    same

--- array.php
+++ array.php
@@ -15,6 +15,21 @@ echo 'The first fristname is '.$students[0]['fname'];
 <h3>Print the whole array</h3>
 <?php
 foreach($students as $row)
-{echo $row['fname'].' '.$['lname'].'<br> ; 
+{echo $row['fname'].' '.$['lname'].'<br>' ; 
 }
 ?>
+<h3>Names in a table</h3>
+
+<table border="1">
+<tr><th>Firstname</th> <th>Lastname</th></tr>
+<?phpforeach($student as $row)
+{
+echo'<tr><td>';
+echo $row['fname'];
+echo '</td><td>';
+echo $row['lname'];
+echo '</td></tr>';
+	
+}
+?>
+</table>
