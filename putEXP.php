91a826db34d0d2d441b330e687fa386fcbd21319
Unknown <heli.isohatala@gmail.com>
2017-03-13 11:08:44 +0200

    joojoo

---
+++ putEXP.php
@@ -0,0 +1,17 @@
+<h2>Put method</h2>
+¨
+<form method="POST" action="putExcample.php">
+<label for="fname">Firstname</label>
+<input type="text" id="fname" name="fname"value="">
+<br>
+<label for="lname">Lastname</label>
+<input type="text" id="lname" name="lname values="">
+<br>
+<input type"submit" name="sendBtn" value="send">
+</form>
+<?php
+$fn=$_POST['fname'];
+$fl=$_POST['lname'];
+echo 'hello '.$fn ' '.$ln
+
+?>
